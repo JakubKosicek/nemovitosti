069fdf67ee3d62a5bf4b9f04dcdea046744f9ac4
Jakub <jakub.kosicek10@sezna.cz>
2024-01-04 11:20:52 +0000

    more accounts

--- nemovitosti/sledovani/tests.py
+++ nemovitosti/sledovani/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PageTestCase(TestCase):
+    def test_display_index(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_display_registration(self):
+        response = self.client.get(reverse("registration"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_registration(self):
+        password = "12345Kombajn"
+        response = self.client.post(
+        reverse("registration"))
+        (
+            "username" = "franta"
+            "password1" = password,
+            "password2" = password,
+
+        ),
+
+        self.assertRedirects(response, reverse("login"))
+        self.assertEqual(response.status_code, 302)
+
+
+
+
